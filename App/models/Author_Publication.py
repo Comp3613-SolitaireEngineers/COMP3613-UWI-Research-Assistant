447ad71ee8dd47909302d2aa35c11d0b2e3740a3
Malakai Frank <143915234+BitNinja101@users.noreply.github.com>
2023-09-24 22:30:49 +0000

    AuthorPublication_Model Implementation

---
+++ App/models/Author_Publication.py
@@ -0,0 +1,21 @@
+from App.database import db
+
+class AuthorPublication(db.Model):
+    __tablename__ = 'author_publication'
+    authorpublication_id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id')) 
+    publication_id = db.Column(db.Integer, db.ForeignKey('publication.id'))  
+
+
+    publications = db.relationship('Publication', secondary='author_publication', back_populates='authors', lazy=True)
+    authors = db.relationship('Author', secondary='author_publication', back_populates='publications', lazy=True)
+
+    def __repr__(self): 
+        return f"<AuthorPublication {self.authorpublication_id}, {self.author_id}, {self.publication_id}>"
+
+    def toJSON(self):
+        return {
+            'authorpublication_id': self.authorpublication_id,
+            'author_id': self.author_id,
+            'publication_id': self.publication_id,
+        }
