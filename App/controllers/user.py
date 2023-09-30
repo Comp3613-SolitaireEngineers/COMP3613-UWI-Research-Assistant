43c42e8f6d633f2d7bc1d9536924adf38ae2bbf1
Allyy02 <98034572+Allyy02@users.noreply.github.com>
2023-09-29 23:01:25 -0400

    update users to be RegularUsers

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,23 +1,23 @@
-from App.models import User
+from App.models import RegularUser
 from App.database import db
 
 def create_user(username, password):
-    newuser = User(username=username, password=password)
+    newuser = RegularUser(username=username, password=password)
     db.session.add(newuser)
     db.session.commit()
     return newuser
 
 def get_user_by_username(username):
-    return User.query.filter_by(username=username).first()
+    return RegularUser.query.filter_by(username=username).first()
 
 def get_user(id):
-    return User.query.get(id)
+    return RegularUser.query.get(id)
 
 def get_all_users():
-    return User.query.all()
+    return RegularUser.query.all()
 
 def get_all_users_json():
-    users = User.query.all()
+    users = RegularUser.query.all()
     if not users:
         return []
     users = [user.get_json() for user in users]
