3bb9445fb3c0d6efd9593b8298257522ed433063
Malakai Frank <143915234+BitNinja101@users.noreply.github.com>
2023-10-01 17:03:20 +0000

    Small edit to controller

--- App/controllers/searchpub.py
+++ App/controllers/searchpub.py
@@ -1,5 +1,5 @@
 from App.models import Author
-from App.models import publication
+from App.models import Publication
 from App.database import db
 
 def search_publications(self,search_term):
@@ -7,8 +7,8 @@ def search_publications(self,search_term):
     author_results = None
     
     if search_term!="":
-          publication_results = publication.query.filter(
-                or_(publication.title.ilike(f'%{search_term}%'), publication.publication_date.ilike(f'%{search_term}%'))
+          publication_results = Publication.query.filter(
+                or_(Publication.title.ilike(f'%{search_term}%'), Publication.publication_date.ilike(f'%{search_term}%'))
           ).all()
 
           author_results = Author.query.filter(
@@ -16,5 +16,5 @@ def search_publications(self,search_term):
           ).all()
   
     else:
-        return "Error: No search term inserted",None
+        return "Error: No search term inserted"
       
